d597b6100cddb0b15100833c7dbc5e4e4df8a689
GerryNwaribe <the.star.is.geraldine@gmail.com>
2024-01-05 01:18:22 +0100

    put previous code

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -3,10 +3,16 @@
 # Check if an argument is provided
 if ARGV.empty?
     puts "Please provide an argument."
-else
-    for a in ARGV do
-        if a.match(/Scho.l/)
-            puts "#{a}"
-        end
+  else
+    input_string = ARGV[0]
+  
+    # Regular expression matching
+    match = input_string.scan(/Scho.l/).flatten
+  
+    # Output the result
+    if match.any?
+        puts match.join
+    else
+      puts "No match"
     end
-end
+  end
